37bb57f376238a7b6e219f105491f3901924f2c0
Luis Medina <i202215560@cibertec.edu.pe>
2025-09-03 21:49:42 -0500

    refactor: generalize myMap function

--- src/phase-1-language-lab/myMap.js
+++ src/phase-1-language-lab/myMap.js
@@ -1,3 +1,7 @@
 export default function myMap(array, callback) {
-  return [];
+  const newArray = [];
+  for (const [index, element] of array.entries()) {
+    newArray.push(callback(element, index, array));
+  }
+  return newArray;
 }
